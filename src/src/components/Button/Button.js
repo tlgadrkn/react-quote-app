2574d5f7959868ee4bda712a179df39360e56765
Tolga Durukan <tolgadurukan16@gmail.com>
2020-05-09 22:04:11 +0300

    Added Tweet Button, button color changes

--- src/src/components/Button/Button.js
+++ src/src/components/Button/Button.js
@@ -1,15 +1,17 @@
  import React from 'react';
  import styles from './button.css';
 
-const Button = (props) => {
+const Button = ({handleQuoteChange, colorToRender, href}) => {
 
     return (
         <div className="btn-group">
-   {props.buttonToRender && 
-            props.buttonToRender
-            .map( (button, index) => (
-                <button onClick={props.handleQuoteChange} key={index}>{button}</button>
-                )) }      
+            <button>
+                <a href={href} className="twitter-share-button" data-show-count="false">
+                    <ion-icon style={{color: colorToRender.backgroundColor}} name="logo-twitter"></ion-icon>
+
+                </a>
+            </button>
+                <button style={{color: colorToRender.backgroundColor}}  onClick={handleQuoteChange}>New Quote</button>
           </div>
     )
     }

9c9f3e496c601b4ec2709a256687e688b5e48d81
Tolga Durukan <tolgadurukan16@gmail.com>
2020-05-09 22:02:02 +0300

    Added Tweet Quote functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import QuoteContainer from './components/QuoteContainer/QuoteContainer';
 
 function App() {
@@ -10,9 +10,6 @@ function App() {
   let [randomQuote, setRandomQuote] = useState([]);
 
   const URL = 'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
-
-  let buttons = ["Tweet This Quote", "New Quote"];
-  
   const backroundColors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
 useEffect(() => {
@@ -34,23 +31,26 @@ useEffect(() => {
 }, [])
 
 
+
   const appColor = {
     backgroundColor: backroundColors[Math.floor(Math.random() * backroundColors.length)]
   }
 
-// Improve handlequoteChange function that it would handle if the quote is same with previous one. 
 
 const handleQuoteChange = () => {
   setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)])
 }
 
+if (isLoading) {
+  return <h2 style= {{color: '#fff'}}>Quotes Are loading</h2>
+ }
+
   return (
     <div className="App" >
-      <Header 
-      colorToRender={appColor}/>
+      {/* <Header 
+      colorToRender={appColor}/> */}
       <QuoteContainer 
        quoteToRender={randomQuote}
-       buttonToRender={buttons} 
        colorToRender={appColor}
        handleQuoteChange={handleQuoteChange}
        />
